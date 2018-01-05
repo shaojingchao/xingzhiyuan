1a53fc951b3a8bb01f82f31de0c394fe27d51779
shaojingchao <1247537710@qq.com>
2018-01-05 18:29:29 +0800

    orientation
    Signed-off-by: shaojingchao <1247537710@qq.com>

--- config/index.js
+++ config/index.js
@@ -13,7 +13,7 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: '192.168.31.31', // can be overwritten by process.env.HOST
+    host: '192.168.4.33', // can be overwritten by process.env.HOST
     port: 3001, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
